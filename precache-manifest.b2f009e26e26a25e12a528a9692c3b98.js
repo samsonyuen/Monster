9ee50f55e3441874324349ecb6861e81d4ff2622
Samson Yuen <samson@Samsons-iMac.local>
2019-12-07 17:18:23 -0800

    Updates

---
+++ precache-manifest.b2f009e26e26a25e12a528a9692c3b98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42225c7885c48f99cda9a54a1545a7d",
+    "url": "/Monster/index.html"
+  },
+  {
+    "revision": "8de72c8c6eec6cca9187",
+    "url": "/Monster/static/css/main.84a45129.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/Monster/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "8de72c8c6eec6cca9187",
+    "url": "/Monster/static/js/main.207c9d69.chunk.js"
+  },
+  {
+    "revision": "d1284b66594e40a06c1a",
+    "url": "/Monster/static/js/runtime-main.1acd6340.js"
+  }
+]);
